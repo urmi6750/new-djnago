f2c591e61aff402f319e83e0713a97caf1387f1f
urmi6750 <fariyahossainurmi@gmail.com>
2020-04-02 01:51:20 +0600

    added css anf html files

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+posts=[
+   {'author':'urmi',
+   'content':'pandemic',
+    'title':'blog -1',
+    'date':'28 th march'
+
+
+},
+   {
+      'author':'fariya',
+      'content':'Govt',
+      'title':'blog -2',
+      'date':'28 th march'
+
+   }
+
+]
 def home (request):
-    return render(request,'blog-app/homepage.html')
+
+    return render(request,'blog-app/homepage.html',context={'posts':posts})
 def about(request):
     return  render(request,'blog-app/about.html')
